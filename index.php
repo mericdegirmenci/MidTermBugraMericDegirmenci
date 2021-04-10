93bdd5737bbb7a593cd9e0bde08cdf027b715f9e
mericdegirmenci <1800003317@stu.iku.edu.tr>
2021-04-10 10:49:35 +0300

    Edited index file

--- index.php
+++ index.php
@@ -3,3 +3,18 @@
 echo "Hello PHP World! I am in a midterm!";
 
 ?>
+
+<form action="form.php" method="POST">
+
+<input type="text" name="licencePlate" value="LicencePlate">
+<input type="text" name="brand" value="Brand">
+<input type="text" name="model" value="Model">
+<input type="text" name="modelYear" value="ModelYear">
+<input type="text" name="color" value="Color">
+
+
+<input type="submit" value="Submit">
+
+</form>
+
+
